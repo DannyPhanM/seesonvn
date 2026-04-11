eb3a20e4a2d038f57cd8bfe14a960a5b9a5aa3aa
DannyPhan <danny@gmail.com>
2026-04-11 14:02:30 +0700

    updated

--- assets/global.js
+++ assets/global.js
@@ -267,7 +267,8 @@ class QuantityInput extends HTMLElement {
     const value = parseInt(this.input.value);
     if (this.input.min) {
       const buttonMinus = this.querySelector(".quantity__button[name='minus']");
-      buttonMinus.classList.toggle('disabled', parseInt(value) <= parseInt(this.input.min));
+      if (buttonMinus) buttonMinus.disabled = parseInt(value) <= parseInt(this.input.min);
+    }
     }
     if (this.input.max) {
       const max = parseInt(this.input.max);
